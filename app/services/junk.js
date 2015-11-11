a2145cfa4db6f053e06daa438cac8b499909b313
Adam Goldman <adamgoldman7@gmail.com>
2015-11-11 16:43:28 +0000

    implement get tags by query

--- app/services/junk.js
+++ app/services/junk.js
@@ -59,34 +59,7 @@ function Junk($q, $localStorage, _DEV, Helpers, CurrentUser, Resource) {
   }
 
   function getTagsByQuery(query, tagsNamesToExclude) {
-    var deferred = $q.defer();
-
-    if (query === 'foo') {
-      log('getTagsByQuery: foo detected! resolving with empty list')
-      deferred.resolve([]) // mock the scenario where no tags match the search query so te use rhas an option to add it
-      return deferred.promise;
-    }
-
-    deferred.resolve( 
-      [
-        {
-          name: 'fizz',
-          linksCount: 5
-        },
-  
-        {
-          name: 'buzz',
-          linksCount: 2
-        },
-  
-        {
-          name: 'pikachu',
-          linksCount: 999
-        }
-      ]
-    );
-
-    return deferred.promise;
+    return Resource.get('tags?query=' + query);
   }
 
   function endorseTag(linkId, tagId) {
