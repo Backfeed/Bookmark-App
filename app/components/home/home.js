832125849256004cb6174debbcef621294c315f7
Adam Goldman <adamgoldman7@gmail.com>
2015-11-19 12:42:01 +0100

    implement search fuzzy search for tags

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -18,9 +18,9 @@ function Home($scope, $timeout, $state,  _DEV, Helpers, Junk) {
     setTagAsSearchQuery: setTagAsSearchQuery,
     refreshTagsToSelectFrom: refreshTagsToSelectFrom,
     addTagToLink: addTagToLink,
-    searchQuery: undefined,
-    searchHasBeenMade: false, // temp until we move search results to hcild state
-    tagsToSelectFrom: [],
+    searchText: undefined,
+    searchHasBeenMade: false, // temp until we move search results to child state
+    selectedSearchTag: undefined,
     linksSearchResults: [],
     tagsSearchResults: []
   });
@@ -35,8 +35,13 @@ function Home($scope, $timeout, $state,  _DEV, Helpers, Junk) {
 
   function search() {
     resetSearchResults();
-    Junk.getLinksAndTagsByQuery(ctrl.searchQuery).then(function(response) {
-      log("links and tags by query", ctrl.searchQuery, response);
+
+    var searchText = ctrl.searchText || ctrl.selectedSearchTag.name;
+
+    log("links and tags by query", searchText);
+
+    Junk.getLinksAndTagsByQuery(searchText).then(function(response) {
+      log("CB links and tags by query", response);
       ctrl.searchHasBeenMade = true;
       ctrl.linksSearchResults = response.links;
       ctrl.tagsSearchResults = response.tags;
@@ -72,32 +77,20 @@ function Home($scope, $timeout, $state,  _DEV, Helpers, Junk) {
 
   function setTagAsSearchQuery(tagName) {
     resetSearchResults();
-    ctrl.searchQuery = tagName;
-    log("links by tag", tagName);
-    Junk.getLinksByTag(tagName).then(function(links) {
-      log("CB: links by tag", links);
-      ctrl.searchHasBeenMade = true;
-      ctrl.linksSearchResults = links;
-    });
+    ctrl.searchText = tagName;
+    ctrl.selectedSearchTag = { name: tagName };
   }
 
   function refreshTagsToSelectFrom(query, tagsToExclude) {
     if (! query)
       return;
 
-    var selectedTagsNamesToExclude = Helpers.mapIds(tagsToExclude);
+    if (tagsToExclude)
+      tagsToExclude = Helpers.mapIds(tagsToExclude);
 
-    log("refreshTagsToSelectFrom by query:", query, "excluding tags", selectedTagsNamesToExclude);
+    log("refreshTagsToSelectFrom by query:", query, "excluding tags", tagsToExclude);
 
-    Junk.getTagsByQuery(query, selectedTagsNamesToExclude).then(function(tags) {
-      log("tags for autocomplete", tags);
-
-      if (tags.length) {
-        ctrl.tagsToSelectFrom = tags;
-      } else {
-        ctrl.tagsToSelectFrom = [{ name: query }]; // give user the option to add a tag that haven't been used yet
-      }
-    });
+    return Junk.getTagsByQuery(query, tagsToExclude);
 
   }
 
